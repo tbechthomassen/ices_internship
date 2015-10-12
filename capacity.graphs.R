a6ac550334084ae7c78a924b8a7f9f71e13bd6fa
Thomas Bech-Thomassen <thomas.bech.thomassen@gmail.com>
2015-10-12 16:49:06 +0200

    Added graphs
    TODO: Find a way to visualise capacity data on y-axis with different
    scale, or somthing to that effect.

--- capacity.graphs.R
+++ capacity.graphs.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(scales)
 library(plyr)
 library(reshape2)
-library(data.table)
+# library(data.table)
 
 effort.data <-
   read.delim(
@@ -17,20 +17,71 @@ gear.list <-
     "~/r/ices/gear.list", header = TRUE, sep = ",", stringsAsFactors = FALSE, fileEncoding = "UTF-8"
   )
 
-
-
 effort.data$gears <- effort.data$Reg_gear
 effort.data$area  <- as.character(effort.data$Reg_area) 
-effort.data$capacity <- effort.data$Fishing_capacity_.GT.
-effort.data$effort_kwd <- effort.data$Effort_.kW.days.
-
+effort.data$capacityGT <- effort.data$Fishing_capacity_.GT.
+effort.data$effortkwd <- effort.data$Effort_.kW.days.
+effort.data$years <- as.factor(effort.data$Year)
 # correcting gears types;
 # translate gear data codes to gear type names 
 gname.apply <- c(gear.list$gear)
 names(gname.apply) <- c(gear.list$data.code)
 effort.data$gears.t <- gname.apply[effort.data$gears]
 
-t.cap.data <- data.table(effort.data)
-setkey(t.cap.data, Year)
-s.year <- t.cap.data[,sum(capacity), by=Year]
+
+s.capacity.effort <-
+  ddply(
+    effort.data, c("area", "gears.t", "years", "Vessel_length", "Country"), summarise,
+    capacity.sum = sum(capacityGT, na.rm = TRUE),
+    effort.sum = sum(effortkwd, na.rm = TRUE)
+  )
+
+m.capacity.effort <-
+  melt(
+    s.capacity.effort, na.rm = TRUE, id.vars = c("area", "years"), measure.vars = c("capacity.sum", "effort.sum")
+  )
+
+#### barplot; landings per guild, summed subregions, no division:
+p.effort.capacity <-
+  ggplot(data = s.capacity.effort, aes(x = years, y = capacity.sum)) #adding data to plot
+p.effort.capacity <-
+  p.effort.capacity + geom_bar(
+    stat = "identity", aes(fill = gears.t), colour =
+      "black", position = "dodge"
+  ) # adding bar-shape and colours + outlines
+p.effort.capacity <- p.effort.capacity + facet_wrap(~area)
+p.effort.capacity <-
+  p.effort.capacity + xlab("Years") + ylab("Fleet capacity (GT)") #Add X- & Y-axis labels
+p.effort.capacity <-
+  p.effort.capacity + ggtitle("Fleet capacity and Effort per gear type and subregion, all years") # setting the title
+p.effort.capacity <-
+  p.effort.capacity + theme(axis.text.x = element_text(angle = 90))
+p.effort.capacity #show the g'damn plot!
+
+#### barplot; landings per guild, summed subregions, no division:
+pm.effort.capacity <-
+  ggplot(data = m.capacity.effort, aes(x = years, y = value)) #adding data to plot
+pm.effort.capacity <-
+  pm.effort.capacity + geom_bar(
+    stat = "identity", aes(fill = area), colour =
+      "black", position = "dodge"
+  ) # adding bar-shape and colours + outlines
+pm.effort.capacity <- pm.effort.capacity + facet_wrap(~variable)
+pm.effort.capacity <-
+  pm.effort.capacity + xlab("Years") + ylab("Fleet capacity (GT)") #Add X- & Y-axis labels
+pm.effort.capacity <-
+  pm.effort.capacity + ggtitle("Fleet capacity and Effort per gear type and subregion, all years") # setting the title
+pm.effort.capacity <-
+  pm.effort.capacity + theme(axis.text.x = element_text(angle = 90))
+pm.effort.capacity #show the g'damn plot!
+
+
+
+
+
+
+
+
+
+
 
