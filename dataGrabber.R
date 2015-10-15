5e60accb12fba65c351b938524f93ff03783c67a
Thomas Bech-Thomassen <thomas.bech.thomassen@gmail.com>
2015-10-15 15:42:03 +0200

    added file check
    The datagrabber now checks if STECF datafiles have already been
    downloaded, and skips the dl step, if they have.

--- dataGrabber.R
+++ dataGrabber.R
@@ -11,29 +11,82 @@ rm(list = ls())
 ############
 #
 needList <- c("openxlsx")
-new.packages <- needList[!(needList %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+new.packages <-
+  needList[!(needList %in% installed.packages()[,"Package"])]
+if (length(new.packages)) {
+  install.packages(new.packages)
+}
 #
 library(openxlsx)
 #
+###############
+# Define dirs #
+###############
+
+cust.tmpd <-
+  "~/r/rdata/download" # define where temporary flies are placed
+
+# check if dir for temp files exists, if not create it:
+if (!dir.exists(cust.tmpd)) {
+  dir.create(cust.tmpd, recursive = TRUE)
+}
+
+
 ##################
 # Load functions #
 ##################
 #
 #
 #
+
 #############
 # Load data #
 #############
 #
-urls <- "http://stecf.jrc.ec.europa.eu/documents/43805/870977/2014_STECF+14-20+-+Fishing+Effort+Regimes+data+tables.zip"
-# 
-tmpFile <- tempfile()
-download.file(urls, destfile = tmpFile, mode = "wb")
-unzip(tmpFile, list = T)
-data <- read.xlsx(unzip(tmpFile, "Electronic_appendices/Landings_by_ICES_rectangle.xlsx"),
-                  sheet = 1)
-unlink(tmpFile)
-head(data)
-save(data)
+effort.url <-
+  "http://stecf.jrc.ec.europa.eu/documents/43805/870977/2014_STECF+14-20+-+Fishing+Effort+Regimes+data+tables.zip"
 #
+
+effort.f <- "~/r/rdata/download/effort.zip"
+
+
+if (!file.exists(effort.f)) {
+  download.file(effort.url, destfile = effort.f, mode = "wb")
+}
+
+unzip(effort.f, list = T)
+effort.data <-
+  read.xlsx(unzip(
+    effort.f, "Electronic_appendices/Landings_by_ICES_rectangle.xlsx"
+  ),
+  sheet = 1)
+head(effort.data)
+write.table(
+  effort.data, file = "~/r/rdata/stecf-landings.by.ICES.rectangle", sep = "\t", quote = FALSE, row.names = FALSE
+)
+
+unlink(effort.data)
+
+econ.url <-
+  "http://stecf.jrc.ec.europa.eu/documents/43805/1034590/2015_STECF+15-07+-+EU+Fleet+Economic+data+tables.zip"
+econ.f <- "~/r/rdata/download/econ.data.zip"
+
+if (!file.exists(econ.f)) {
+  download.file(econ.url, destfile = econ.f, mode = "wb")
+}
+
+unzip(econ.f, list = T)
+econ.data <-
+  read.xlsx(
+    unzip(
+      econ.f, "STECF 15-07_2015_EU Fleet Economic and Transversal data_fleet segment level.xlsx"
+    ),
+    sheet = 2
+  )
+
+head(econ.data)
+
+write.table(
+  econ.data, file = "~/r/rdata/stecf-economic-fleet.data", sep = "\t", quote = FALSE, row.names = FALSE
+)
+unlink(econ.data)
